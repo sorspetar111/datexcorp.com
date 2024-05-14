6b0960862bb9677790a0980d7774f71d0bf1477e
sorspetar111 <156424190+sorspetar111@users.noreply.github.com>
2024-05-14 21:16:13 +0300

    Update Models.cs

--- Models.cs
+++ Models.cs
@@ -1,3 +1,4 @@
+// Clasic one version
 public class Pallet
 {
     public int Id { get; set; }
@@ -11,4 +12,29 @@ public class Box
     public bool IsOpened { get; set; }
     public int? PalletId { get; set; }
     public int? ParentBoxId { get; set; }
+}
+
+
+
+// This is EF version of models
+public class Pallet
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    // Navigation property
+    public ICollection<Box> Boxes { get; set; }
+}
+
+public class Box
+{
+    public int Id { get; set; }
+    public string Barcode { get; set; }
+    public bool IsOpened { get; set; }
+    public int? PalletId { get; set; }
+    public int? ParentBoxId { get; set; }
+  
+    public Box ParentBox { get; set; }
+    public Pallet Pallet { get; set; }
+    public ICollection<Box> ChildrenBoxes { get; set; }
 }
